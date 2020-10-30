118e639475367cdf1dca76c1d91cf6f97a986056
Abu Hassan <abu.a.hassan@gmail.com>
2020-10-30 10:04:21 +0800

    database transactions to protect db

--- app/Http/Controllers/OrderPaymentController.php
+++ app/Http/Controllers/OrderPaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Services\CartService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderPaymentController extends Controller
 {
@@ -19,7 +20,7 @@ class OrderPaymentController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @param  \App\Models\Order  $order
+     * @param  \App\Order  $order
      * @return \Illuminate\Http\Response
      */
     public function create(Order $order)
@@ -33,26 +34,27 @@ class OrderPaymentController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Order  $order
+     * @param  \App\Order  $order
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request, Order $order)
     {
-        //PaymentService::handlePayment()
+        return DB::transaction(function() use($order) {
+            //PaymentService::handlePayment()
 
-        $this->cartService->getFromCookie()->products()->detach();
+            $this->cartService->getFromCookie()->products()->detach();
 
-        $order->payment()->create([
-            'amount' => $order->total,
-            'payed_at' => now(),
-        ]);
+            $order->payment()->create([
+                'amount' => $order->total,
+                'payed_at' => now(),
+            ]);
 
-        $order->status = 'payed';
-        $order->save();
+            $order->status = 'payed';
+            $order->save();
 
-        return redirect()
-            ->route('main')
-            ->withSuccess("Thank you! We received your payment of Rm{$order->total}.");
+            return redirect()
+                ->route('main')
+                ->withSuccess("Thanks! We received your \${$order->total} payment.");
+        });
     }
-
 }
